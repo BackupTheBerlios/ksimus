339028ddcb9cdea18bc665624feb7e64f82607a4
rasmus <rasmus>
2001-12-01 23:46:20 +0000

    * New class ConnectorPack: This class handles multiple connectors. It places the connectors   and add a add and a delete entry to the popup menu.

---
+++ ksimus/ksimus/items/connectorpack.cpp
@@ -0,0 +1,342 @@
+/***************************************************************************
+                          connectorpack.cpp  -  description
+                             -------------------
+    begin                : Tue Nov 27 2001
+    copyright            : (C) 2001 by Rasmus Diekenbrock
+    email                : ksimus@gmx.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+// C-Includes
+
+// QT-Includes
+#include <qpopupmenu.h>
+
+// KDE-Includes
+#include <klocale.h>
+
+// Project-Includes
+#include "ksimdebug.h"
+#include "connectorbase.h"
+#include "connectorinfo.h"
+#include "component.h"
+#include "ksimdata.h"
+#include "connectorpack.h"
+
+
+// Forward declaration
+
+
+#define USE_LETTER   0x0001L
+
+
+static const char * sGroup          = "connPack/%1/";
+static const char * sConnectorCount = "Connector Count";
+static const char * sConn           = "Conn%1/";
+
+
+
+
+ConnectorPack::ConnectorPack(const QString & name, Component * comp, const ConnectorInfo * connInfo,
+                              unsigned int minConn = 1, unsigned int maxConn = 10)
+	:	ComponentAddOn(comp),
+		m_name(name),
+		m_connInfo(connInfo),
+		m_minConn(minConn),
+		m_maxConn(maxConn),
+		m_connName(),
+		m_orientation(CO_LEFT),
+		m_flags(USE_LETTER)
+{
+	m_connList = new ConnectorList();
+	CHECK_PTR(m_connList);
+//	m_connList->setAutoDelete(true);
+
+}
+
+ConnectorPack::~ConnectorPack()
+{
+}
+
+ConnectorBase * ConnectorPack::internalAddConnector()
+{
+	ConnectorBase * conn;
+	
+	if(getConnectorMaximum() > getConnectorCount())
+	{
+		conn = getConnectorInfo()->create(getComponent(), createNewName(), QPoint());
+		conn->getAction().disable(KSimAction::STORAGE);
+		conn->setOrientation(getOrientation());
+		
+		getConnList()->append(conn);
+		emit signalAddConnector(conn);
+	}
+	else
+	{
+		conn = 0;
+	}
+	
+	return conn;
+}
+	
+bool ConnectorPack::internalDeleteConnector()
+{
+	ConnectorBase * conn = getDeletableConnector();
+	bool res;
+	
+	if ((getConnectorMinimum() < getConnectorCount()) && (conn))
+	{
+		emit signalDeleteConnector(conn);
+		getConnList()->removeRef(conn);
+		getComponent()->getConnList()->removeRef(conn);
+		emit signalDeletedConnector();
+		res = true;
+	}
+	else
+	{
+		res = false;
+	}
+	
+	return res;
+}
+
+void ConnectorPack::setConnectorCount(unsigned int connCount)
+{
+	bool deleted = true;
+	
+	KSIMDEBUG_VAR("setConnectorCount (1)",getConnectorCount());
+	
+	// Limit connector count
+	if (connCount > getConnectorMaximum())
+	{
+		KSIMDEBUG(QString("ConnectorPack::setConnectorCount greater than max (%1>%2)")
+								.arg(connCount).arg(getConnectorMaximum()));
+		connCount = getConnectorMaximum();
+	}
+	if (connCount < getConnectorMinimum())
+	{
+		KSIMDEBUG(QString("ConnectorPack::setConnectorCount lesser than min (%1<%2)")
+								.arg(connCount).arg(getConnectorMinimum()));
+		connCount = getConnectorMinimum();
+	}
+	
+	do
+	{
+		deleted = internalDeleteConnector();
+	} while((connCount < getConnectorCount()) && (deleted));
+	
+	while(connCount > getConnectorCount())
+	{
+		internalAddConnector();
+	};
+	
+	getComponent()->refresh();
+}
+
+unsigned int  ConnectorPack::getConnectorCount() const
+{
+	return getConnList()->count();
+}
+	
+void ConnectorPack::setConnectorMinimum(unsigned int minConn)
+{
+	m_minConn = minConn;	
+}
+
+void ConnectorPack::setConnectorMaximum(unsigned int maxConn)
+{
+	m_maxConn = maxConn;	
+}
+
+void ConnectorPack::slotAddConnector()
+{
+	getComponent()->undoChangeProperty(i18n("Add connector"));
+	internalAddConnector();
+}
+
+void ConnectorPack::slotDeleteConnector()
+{
+	getComponent()->undoChangeProperty(i18n("Delete connector"));
+	internalDeleteConnector();
+}
+
+void ConnectorPack::slotDeleteConnector(ConnectorBase * conn)
+{
+	if ((getConnList()->findRef(conn) != 0) && (conn->getWire() == 0))
+	{
+		getComponent()->undoChangeProperty(i18n("Delete connector"));
+		internalDeleteConnector();
+		getComponent()->refresh();
+	}		
+}
+	
+QString ConnectorPack::createNewName()
+{
+	unsigned int i = 0;
+	bool found;
+	QString name;
+	
+	do
+	{
+		found = false;
+		i++;
+		
+		if (getConnectorName() == QString())
+		{
+			name = getName() + " %1";
+		}
+		else
+		{
+			name = getConnectorName();
+		}
+		
+		
+		if (isLetter())
+		{
+			name = name.arg(QChar((char)i + 'A' - 1));
+		}
+		else
+		{
+			name = name.arg(i);
+		}
+		
+		
+		
+		FOR_EACH_CONNECTOR(it, *getConnList())
+		{
+			if (it.current()->getName() == name)
+			{
+				found = true;
+				break;
+			}
+		}
+	} while (found);
+	return name;
+}
+
+ConnectorBase * ConnectorPack::getDeletableConnector()
+{
+	QListIterator<ConnectorBase> it(*getConnList());
+	
+	ConnectorBase * conn = it.toLast();
+	
+	if ((conn) && (conn->getWire() == 0))
+	{
+		return conn;
+	}
+	return 0;
+}
+
+bool ConnectorPack::initPopupMenu(QPopupMenu * popup)
+{
+	int i;
+	
+	ComponentAddOn::initPopupMenu(popup);
+	KSIMDEBUG_VAR("",getConnectorCount());
+	
+	popup->insertSeparator();
+		
+	i = popup->insertItem("Add "+ getName(), this, SLOT(slotAddConnector()));
+	if(getConnectorCount() >= getConnectorMaximum())
+		popup->setItemEnabled(i, false);
+		
+	i = popup->insertItem("Delete "+ getName(), this, SLOT(slotDeleteConnector()));
+	if((getConnectorCount() <= getConnectorMinimum()) || (getDeletableConnector() == 0))
+		popup->setItemEnabled(i, false);
+
+	return true;
+}
+
+void ConnectorPack::save(KSimData & file) const
+{
+	QString oldGroup(file.group());
+	QString group((oldGroup + sGroup).arg(getName()));	
+	file.setGroup(group);
+	
+	file.writeEntry(sConnectorCount,getConnectorCount());
+	
+	unsigned int c = 0;
+	FOR_EACH_CONNECTOR(it, *getConnList())
+	{
+		QString connGroup(QString(sConn).arg(c));
+		
+		connGroup = group + connGroup;
+		file.setGroup(connGroup);
+		it.current()->save(file);
+		c++;
+	}
+	
+	file.setGroup(oldGroup);
+}
+
+bool ConnectorPack::load(KSimData & file)
+{
+	unsigned int c;
+	QString oldGroup(file.group());
+	QString group((oldGroup + sGroup).arg(getName()));	
+	file.setGroup(group);
+	
+	c = file.readUnsignedNumEntry(sConnectorCount);
+	setConnectorCount(c);
+		
+	c = 0;
+	FOR_EACH_CONNECTOR(it, *getConnList())
+	{
+		QString connGroup(QString(sConn).arg(c));
+		
+		connGroup = group + connGroup;
+		file.setGroup(connGroup);
+		it.current()->load(file);
+		c++;
+	}
+	
+	file.setGroup(oldGroup);
+	
+	return true;
+}
+
+void ConnectorPack::setConnectorName(const QString & connName)
+{
+	m_connName = connName;
+}
+
+QString ConnectorPack::getConnectorName() const
+{
+	return m_connName;
+}
+
+void ConnectorPack::setLetter(bool letter)
+{
+	if (letter)
+	{
+		m_flags |= USE_LETTER;
+	}
+	else
+	{
+		m_flags &= ~USE_LETTER;
+	}
+}
+	
+bool ConnectorPack::isLetter() const
+{
+	return m_flags & USE_LETTER;
+}
+
+/** Sets the orientation of the connectors. */
+void ConnectorPack::setOrientation(ConnOrientationType orient)
+{
+	m_orientation = orient;
+	
+	FOR_EACH_CONNECTOR(it, *getConnList())
+	{
+		it.current()->setOrientation(orient);
+	}
+}
---
+++ ksimus/ksimus/items/connectorpack.h
@@ -0,0 +1,187 @@
+/***************************************************************************
+                          connectorpack.h  -  description
+                             -------------------
+    begin                : Tue Nov 27 2001
+    copyright            : (C) 2001 by Rasmus Diekenbrock
+    email                : ksimus@gmx.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef CONNECTORPACK_H
+#define CONNECTORPACK_H
+
+// C-Includes
+
+// QT-Includes
+
+// KDE-Includes
+
+// Project-Includes
+#include "connectorbase.h"
+#include "componentaddon.h"
+#include "types.h"
+
+
+// Forward declaration
+class ConnectorInfo;
+
+
+/**Class which support your component with a variable number of connectors.
+
+  *@author Rasmus Diekenbrock
+  */
+
+class ConnectorPack : public ComponentAddOn
+{
+  Q_OBJECT
+
+public:
+	
+	/** Constructs a new connector pack.
+		*
+		* @param name     The name is used for the storages of the connector pack. It is also used as default
+		*                 connector name.
+		* @param comp     The parent component.
+		* @param connInfo The info (means type) of the connector to add by this connector pack.
+		* @param minConn  The minimum count of connectors. The default is one connector.
+		* @param maxConn  The maximum count of connectors. The default are ten connecotrs.
+		*/
+	ConnectorPack(const QString & name, Component * comp, const ConnectorInfo * connInfo,
+	              unsigned int minConn = 1, unsigned int maxConn = 10);
+	/** The destructor.
+	 */
+	~ConnectorPack();
+
+	/** Returns the list of the current connectors.
+	 */
+	ConnectorList * getConnList() const { return m_connList; };
+		
+	/** Sets the current count of connectors. Connecotrs are created or deleted if the count not match.
+	 */
+	void setConnectorCount(unsigned int connCount);
+	/** Returns the current count of connectors.
+	 */
+	unsigned int getConnectorCount() const;
+	
+	/** Sets the minimum count of connectors. The function creates no missing connectors!
+	 */
+	void setConnectorMinimum(unsigned int minConn);
+	/** Returns the minimum count of connectors.
+	 */
+	unsigned int getConnectorMinimum() const { return m_minConn; };
+	
+	/** Sets the maximum count of connectors. The function delete no supernumeray connectors!
+	 */
+	void setConnectorMaximum(unsigned int minConn);
+	/** Returns the maximum count of connectors.
+	 */
+	unsigned int getConnectorMaximum() const { return m_maxConn; };
+	
+  /** Returns the info (means type) of the connector.
+   */
+  const ConnectorInfo * getConnectorInfo() const { return m_connInfo; };
+  /** Returns the name of this connector pack. It is also used as default connector name. */
+	QString getName() const { return m_name; };
+
+	/** Initialize the component popup menu.
+	  *	Return true, if items are added.
+		* This function takes care about the KSimAction information.
+		*/
+	bool initPopupMenu(QPopupMenu * popup);
+	/** Save component add on's properties.
+		* The default implementation does nothing.
+		*/
+	virtual void save(KSimData & file) const;
+	/** Load component add on's properties.
+		*	Returns true if successful.
+		* The default implementation does nothing.
+		*/
+	virtual bool load(KSimData & file);
+	
+	/** Set the connector name here. The set connector name before calling first @see setConnectorCount
+	  * Don't forget the following %1.
+	  */
+	void setConnectorName(const QString & connName);
+	/** Returns the current connector name.
+	  */
+	QString getConnectorName() const;
+	
+	/** The connectors are named with letters if ture, or with numbers is false. The default are letters. */
+	void setLetter(bool letter);
+	/** Returns ture if the connector are named with letter, or false if named with numbers. */
+	bool isLetter() const;
+	/** Sets the orientation of the connectors. */
+  void setOrientation(ConnOrientationType orient);
+  /** Returns the orientations of the connectors. */
+  ConnOrientationType getOrientation() const { return m_orientation; };
+
+public slots:
+	/** Call this slot to add a connector. The slot is not successful if the maximum count is reached. */
+	void slotAddConnector();
+	/** Call this slot to delete a connector. The slot tests if one or the last connector is not connected
+	  * and deletes it. */
+	void slotDeleteConnector();
+	/** Call this slot to delete a certain connector. The slot does nothing, if the pack don't contain the
+	  * connector or the miminim connector count is reached.
+	  */
+	void slotDeleteConnector(ConnectorBase * conn);
+
+protected:
+	/** The function creates a name for a new connector. Per default it is the pack name and a letter or
+	  * a number depenning on the setting of @see setLetter.
+	  */
+	virtual QString createNewName();
+	/** Returns the next connector to delete or zero if all connector are connected.
+	  */
+	virtual ConnectorBase * getDeletableConnector();
+		
+private: 		//functions
+	/** This internal function adds a new connector.
+	  *
+	  * It tests the maximum connector count (set by @see setConnectorMaximum) and if the maximum is
+	  * not reached it creates a new connector and after that emits the signal @see signalAddConnector.
+	  * The function returns a pointer to the created connector.
+	  */
+	ConnectorBase * internalAddConnector();
+	
+	/** This functions deletes a coonnector.
+	  *
+	  * The function tests if one or the last connector is not connected and deletets it. The signal
+	  * @see signalDeleteConnector is emitted before the connector is deleted and the signal
+	  * @see signalDeletedConnector is emitted after the connector is deleted. The function returns
+	  * true, if a connector is deleted and false if no connector is deleted.
+	  */
+	bool internalDeleteConnector();
+
+private:
+	QString m_name;
+	ConnectorList * m_connList;
+	const ConnectorInfo * m_connInfo;
+	unsigned int m_minConn;
+	unsigned int m_maxConn;
+	QString m_connName;
+	ConnOrientationType m_orientation;
+	Q_UINT32 m_flags;
+	
+signals:
+	/** This signal is emitted after a connectoer is created.
+	  */
+	void signalAddConnector(ConnectorBase * conn);
+	/** This signal is emitted before a connector is deleted.
+	  */
+	void signalDeleteConnector(ConnectorBase * conn);
+	/** This signal is emitted after a connector is deleted.
+	  */
+	void signalDeletedConnector();
+
+};
+
+#endif
