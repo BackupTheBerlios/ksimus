86624b15508f2d32e97ed660e9c9792f5f5438a9
rasmus <rasmus>
2001-12-01 23:46:34 +0000

    * New class ComponentLayout: This class is a component add on and arranges the connectors   and resize the component view.

---
+++ ksimus/ksimus/items/componentlayout.cpp
@@ -0,0 +1,570 @@
+/***************************************************************************
+                          complayout.cpp  -  description
+                             -------------------
+    begin                : Sat Nov 24 2001
+    copyright            : (C) 2001 by Rasmus Diekenbrock
+    email                : ksimus@gmx.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+// C-Includes
+
+// QT-Includes
+
+// KDE-Includes
+
+// Project-Includes
+#include "ksimdebug.h"
+#include "connectorbase.h"
+#include "component.h"
+#include "compcontainer.h"
+#include "compview.h"
+#include "resource.h"
+#include "componentlayout.h"
+#include "connectorpack.h"
+
+// Forward declaration
+
+//#######################################################################
+//#######################################################################
+
+CompLayoutBaseItem::CompLayoutBaseItem(CompLayoutBase * parentLayout)
+	:	m_parentLayout(parentLayout)
+{
+}
+
+CompLayoutBaseItem::~CompLayoutBaseItem()
+{
+}
+
+ComponentLayout * CompLayoutBaseItem::getBaseLayout() const
+{
+	return getParent()->getBaseLayout();
+}
+
+Component * CompLayoutBaseItem::getComponent() const
+{
+	return getBaseLayout()->getComponent();
+}
+
+//#######################################################################
+//#######################################################################
+
+class CompLayoutConnector : public CompLayoutBaseItem
+{
+public:
+	CompLayoutConnector(CompLayoutBase * parentLayout, ConnectorBase * conn, int additionalSpace = 1)
+		:	CompLayoutBaseItem(parentLayout),
+			m_conn(conn),
+			m_space(QMAX(0,additionalSpace))
+	{};
+	
+	ConnectorBase * getConnector() const { return m_conn;};
+	int getAdditionalSpace() const { return m_space;};
+	
+	virtual int getSize() const;
+	virtual int getStretchFactor() const;
+	virtual int position(int & size, int & sumStretch, QPoint startPos);
+
+
+private:
+	ConnectorBase * m_conn;
+	int m_space;
+};
+
+int CompLayoutConnector::getSize() const
+{
+	if (getConnector()->isHidden())
+	{
+		return 0;
+	}
+	else
+	{
+		return 1 + getAdditionalSpace();
+	}
+}
+
+int CompLayoutConnector::getStretchFactor() const
+{
+	return 0;
+}
+
+int CompLayoutConnector::position(int & /*size*/, int & /*sumStretch*/, QPoint startPos)
+{
+	if (getConnector()->isHidden())
+	{
+		getConnector()->setGridPos(QPoint());
+		return 0;
+	}
+	else
+	{
+		getConnector()->setGridPos(startPos);
+		return 1 + getAdditionalSpace();
+	}
+}
+
+//#######################################################################
+//#######################################################################
+
+class CompLayoutSpace : public CompLayoutBaseItem
+{
+public:
+	CompLayoutSpace(CompLayoutBase * parentLayout, int space)
+		: CompLayoutBaseItem(parentLayout),
+			m_space(QMAX(0,space))
+	{};
+	
+	int getSpace() const { return m_space;};
+	
+	
+	virtual int getSize() const;
+	virtual int getStretchFactor() const;
+	virtual int position(int & size, int & sumStretch, QPoint startPos);
+
+private:
+	int m_space;
+};
+
+int CompLayoutSpace::getSize() const
+{
+	return getSpace();
+}
+
+int CompLayoutSpace::getStretchFactor() const
+{
+	return 0;
+}
+
+int CompLayoutSpace::position(int & /*size*/, int & /*sumStretch*/, QPoint /*startPos*/)
+{
+	return getSpace();
+}
+
+//#######################################################################
+//#######################################################################
+
+class CompLayoutStretch : public CompLayoutBaseItem
+{
+public:
+	CompLayoutStretch(CompLayoutBase * parentLayout, int factor)
+		: CompLayoutBaseItem(parentLayout),
+			m_factor(QMAX(1,factor))
+	{};
+	
+	int getFactor() const { return m_factor;};
+	
+	virtual int getSize() const;
+	virtual int getStretchFactor() const;
+	virtual int position(int & size, int & sumStretch, QPoint startPos);
+
+private:
+	int m_factor;
+};
+
+int CompLayoutStretch::getSize() const
+{
+	return 0;
+}
+
+int CompLayoutStretch::getStretchFactor() const
+{
+	return getFactor();
+}
+
+int CompLayoutStretch::position(int & size, int & sumStretch, QPoint /*startPos*/)
+{
+	if (sumStretch == 0)
+	{
+		return 0;
+		KSIMDEBUG("sumStretch == 0");
+	}
+	else
+	{
+		int stretch = (int)(((double)(size * getFactor()) / sumStretch) + 0.5);
+		size -= stretch;
+		sumStretch -= getFactor();
+		return stretch;
+	}
+}
+
+//#######################################################################
+//#######################################################################
+
+class CompLayoutConnPack : public CompLayoutBaseItem
+{
+public:
+	CompLayoutConnPack(CompLayoutBase * parentLayout, ConnectorPack * connPack, int additionalSpace = 1)
+		: CompLayoutBaseItem(parentLayout),
+			m_connPack(connPack),
+		  m_space(QMAX(0,additionalSpace))
+	{
+	};
+	
+	ConnectorPack * getConnectorPack() const { return m_connPack;};
+	int getAdditionalSpace() const { return m_space;};
+	
+	
+	virtual int getSize() const;
+	virtual int getStretchFactor() const;
+	virtual int position(int & size, int & sumStretch, QPoint startPos);
+
+private:
+	ConnectorPack * m_connPack;
+	int m_space;
+};
+
+int CompLayoutConnPack::getSize() const
+{
+	int i = 0;
+	
+	FOR_EACH_CONNECTOR(it, *getConnectorPack()->getConnList())
+	{
+		if (!it.current()->isHidden())
+		{
+			i+= 1;
+		}
+	}
+	return i * ( 1 + getAdditionalSpace());
+}
+
+int CompLayoutConnPack::getStretchFactor() const
+{
+	return 0;
+}
+
+int CompLayoutConnPack::position(int & /*size*/, int & /*sumStretch*/, QPoint startPos)
+{
+	int i = 0;
+	
+	FOR_EACH_CONNECTOR(it, *getConnectorPack()->getConnList())
+	{
+		if (!it.current()->isHidden())
+		{
+			it.current()->setGridPos(startPos);
+			i+= 1 + getAdditionalSpace();
+			getParent()->addPosition(startPos, 1 + getAdditionalSpace());
+		}
+	}
+	return i;
+}
+
+
+
+//#######################################################################
+//#######################################################################
+
+#define BASE_LAYOUT 0x0001
+
+	
+CompLayoutBase::CompLayoutBase(CompLayoutBase * parentLayout, ConnOrientationType orientation)
+	:	CompLayoutBaseItem(parentLayout),
+		m_orientation(orientation),
+		m_flags(0)
+{
+	m_parent.layout = parentLayout;
+	
+	m_itemList = new CompLayoutList();
+	m_itemList->setAutoDelete(true);
+}
+
+CompLayoutBase::CompLayoutBase(ComponentLayout * parent, ConnOrientationType orientation)
+	:	CompLayoutBaseItem(0),
+		m_orientation(orientation),
+		m_flags(BASE_LAYOUT)
+{
+	m_parent.base = parent;
+	
+	m_itemList = new CompLayoutList();
+	m_itemList->setAutoDelete(true);
+}
+
+CompLayoutBase::~CompLayoutBase()
+{
+	delete m_itemList;
+}
+
+CompLayoutBase * CompLayoutBase::getParentLayout() const
+{
+	if (m_flags & BASE_LAYOUT)
+	{
+		return 0;
+	}
+	else
+	{
+		return m_parent.layout;
+	}
+};
+
+ComponentLayout * CompLayoutBase::getBaseLayout() const
+{
+	if (m_flags & BASE_LAYOUT)
+	{
+		return m_parent.base;
+	}
+	else
+	{
+		return m_parent.layout->getBaseLayout();
+	}
+}
+
+Component * CompLayoutBase::getComponent() const
+{
+	return getBaseLayout()->getComponent();
+}
+
+void CompLayoutBase::addLayout(CompLayoutBase * layout)
+{
+	if (layout->getOrientation() != getOrientation())
+	{
+		layout->setOrientation(getOrientation());
+		
+		getBaseLayout()->logError("CompLayoutBase::addLayout Orientation missmatch!");
+	}
+	
+	getItemList()->append(layout);
+}
+
+void CompLayoutBase::addConnector(ConnectorBase * conn, int additionalSpace = 1)
+{
+	CompLayoutBaseItem * item = new CompLayoutConnector(this, conn, additionalSpace);
+	CHECK_PTR(item);
+	
+	getItemList()->append(item);
+	
+	conn->setOrientation(getOrientation());
+	QObject::connect(conn, SIGNAL(signalProperty()),getBaseLayout(),SLOT(updateLayout()));
+}
+
+void CompLayoutBase::addSpace(int space)
+{
+	CompLayoutBaseItem * item = new CompLayoutSpace(this, space);
+	CHECK_PTR(item);
+	
+	getItemList()->append(item);
+}
+
+void CompLayoutBase::addStretch(int factor)
+{
+	CompLayoutBaseItem * item = new CompLayoutStretch(this, factor);
+	CHECK_PTR(item);
+	
+	getItemList()->append(item);
+}
+
+void CompLayoutBase::addConnectorPack(ConnectorPack * connPack, int space = 1)
+{
+	CompLayoutBaseItem * item = new CompLayoutConnPack(this, connPack, space);
+	CHECK_PTR(item);
+	
+	connPack->setOrientation(getOrientation());
+	
+	getItemList()->append(item);
+	QObject::connect(connPack, SIGNAL(signalAddConnector(ConnectorBase *)),getBaseLayout(),SLOT(updateLayout()));
+	QObject::connect(connPack, SIGNAL(signalDeletedConnector()),getBaseLayout(),SLOT(updateLayout()));
+}
+
+int CompLayoutBase::getSize() const
+{
+	int res = 0;
+	
+	FOR_EACH_LAYOUTITEM(it,*getItemList())
+	{
+		res += it.current()->getSize();
+	}
+	return res;
+}
+
+int CompLayoutBase::getStretchFactor() const
+{
+	int res = 0;
+	
+	FOR_EACH_LAYOUTITEM(it,*getItemList())
+	{
+		res += it.current()->getStretchFactor();
+	}
+	return res;
+}
+
+int CompLayoutBase::position(int & size, int & sumStretch, QPoint startPos)
+{
+	int res = 0;
+	
+	FOR_EACH_LAYOUTITEM(it,*getItemList())
+	{
+		int offset = it.current()->position(size, sumStretch, startPos);
+		addPosition(startPos, offset);
+		res += offset;
+	}
+	return res;
+}
+
+void CompLayoutBase::addPosition(QPoint & pos, int add)
+{
+	switch(getOrientation())
+	{
+		case CO_TOP:
+		case CO_BOTTOM:
+			pos.rx() += add;
+			break;
+		case CO_LEFT:
+		case CO_RIGHT:
+			pos.ry() += add;
+			break;
+		
+		default:
+			KSIMDEBUG_VAR("Unknown orientation", getOrientation());
+	}
+}
+
+//#######################################################################
+//#######################################################################
+
+
+//#######################################################################
+//#######################################################################
+
+
+ComponentLayout::ComponentLayout(Component * component)
+	:	ComponentAddOn(component),
+		m_minSize(QSize(5,5)),
+		m_currentSize(QSize()),
+		m_fixedSize(false)
+{
+	
+	getAction().disable(KSimAction::ALL);
+	
+	
+	m_left   = new CompLayoutBase(this, CO_LEFT);
+	CHECK_PTR(m_left);
+	m_right  = new CompLayoutBase(this, CO_RIGHT);
+	CHECK_PTR(m_right);
+	m_top    = new CompLayoutBase(this, CO_TOP);
+	CHECK_PTR(m_top);
+	m_bottom = new CompLayoutBase(this, CO_BOTTOM);
+	CHECK_PTR(m_bottom);
+
+
+	connect(getComponent()->getSheetView(), SIGNAL(signalResize(const QSize &)),
+					this, SLOT(slotResizeView()));
+}
+
+
+ComponentLayout::~ComponentLayout()
+{
+	delete m_bottom;
+	delete m_top;
+	delete m_right;
+	delete m_left;
+}
+
+void ComponentLayout::updateLayout()
+{
+	int leftSize, rightSize, topSize, bottomSize;
+	int leftSumStretch, rightSumStretch, topSumStretch, bottomSumStretch;
+	
+	bool leftConn, rightConn, topConn, bottomConn;
+	int sizeX, sizeY;
+	
+	
+	// Component side used?
+	leftConn = getLeft()->getItemList()->count() != 0;
+	rightConn = getRight()->getItemList()->count() != 0;
+	topConn = getTop()->getItemList()->count() != 0;
+	bottomConn = getBottom()->getItemList()->count() != 0;
+	
+	getComponent()->getSheetView()->enableConnectorSpacingLeft(leftConn);
+	getComponent()->getSheetView()->enableConnectorSpacingRight(rightConn);
+	getComponent()->getSheetView()->enableConnectorSpacingTop(topConn);
+	getComponent()->getSheetView()->enableConnectorSpacingBottom(bottomConn);
+	
+	
+	leftSize         = getLeft()->getSize();
+	leftSumStretch   = getLeft()->getStretchFactor();
+	rightSize        = getRight()->getSize();
+	rightSumStretch  = getRight()->getStretchFactor();
+	topSize          = getTop()->getSize();
+	topSumStretch    = getTop()->getStretchFactor();
+	bottomSize       = getBottom()->getSize();
+	bottomSumStretch = getBottom()->getStretchFactor();
+
+  if (isFixedSize())
+  {
+  	sizeX = getComponent()->getSheetView()->getPlace().width() / gridX;
+  	sizeY = getComponent()->getSheetView()->getPlace().height() / gridY;
+  }
+  else
+  {
+  	sizeX = QMAX(topSize,bottomSize);
+		sizeY = QMAX(leftSize,rightSize);
+		
+		if (leftConn) sizeX++;
+		if (rightConn) sizeX++;
+		if (topConn) sizeY++;
+		if (bottomConn) sizeY++;
+
+  	sizeX = QMAX(sizeX,getMinSize().width());
+		sizeY = QMAX(sizeY,getMinSize().height());
+		
+		m_currentSize = QSize(sizeX * gridX, sizeY * gridY);
+		
+		QRect newPlace(getComponent()->getSheetView()->getPos(), m_currentSize);
+		QSize mapSize(getComponent()->getContainer()->getSheetSize());
+		
+		if (newPlace.bottom() > mapSize.height())
+			newPlace.moveBy(0, mapSize.height() - newPlace.bottom());
+		
+		if (newPlace.right() > mapSize.width())
+			newPlace.moveBy(mapSize.width() - newPlace.right(), 0);
+		
+		if (newPlace.top() < 0)
+			newPlace.moveBy(0, - newPlace.top());
+		
+		if (newPlace.left() < 0)
+			newPlace.moveBy( -newPlace.left(), 0);
+		
+		
+		getComponent()->getSheetView()->setPlace(newPlace);
+	}
+	
+ 	int i;
+ 	i = sizeY - leftSize;
+ 	getLeft()->position(i, leftSumStretch, QPoint(0,topConn?1:0));
+ 	i = sizeY - rightSize;
+ 	getRight()->position(i, rightSumStretch, QPoint(sizeX-1,topConn?1:0));
+ 	i = sizeX - topSize;
+ 	getTop()->position(i, topSumStretch, QPoint(leftConn?1:0,0));
+ 	i = sizeX - bottomSize;
+ 	getBottom()->position(i, bottomSumStretch, QPoint(leftConn?1:0,sizeY-1));
+}
+
+
+void ComponentLayout::slotResizeView()
+{
+	if (getComponent()->getSheetView()->getPlace().size() != m_currentSize)
+	{
+		updateLayout();
+	}
+}
+
+
+
+void ComponentLayout::setMinSize(const QSize & minSize)
+{
+	m_minSize = minSize;
+}
+	
+
+void ComponentLayout::setFixedSize(bool fixed)
+{
+  m_fixedSize = fixed;
+}
+
+
---
+++ ksimus/ksimus/items/componentlayout.h
@@ -0,0 +1,332 @@
+/***************************************************************************
+                          complayout.h  -  description
+                             -------------------
+    begin                : Sat Nov 24 2001
+    copyright            : (C) 2001 by Rasmus Diekenbrock
+    email                : ksimus@gmx.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef COMPLAYOUT_H
+#define COMPLAYOUT_H
+
+// C-Includes
+
+// QT-Includes
+#include <qlist.h>
+
+// KDE-Includes
+
+// Project-Includes
+#include "types.h"
+#include "componentaddon.h"
+
+
+// Forward declaration
+class CompLayoutConnector;
+class CompLayoutSpace;
+class CompLayoutStretch;
+class ConnectorBase;
+class ComponentLayout;
+class ConnectorPack;
+class CompLayoutBase;
+
+
+//#######################################################################
+//#######################################################################
+
+/** Smallest item class for component layouts. All layout elements are based on this class.
+  * This class defines the base API of all layout classes. The class itself does nothing.
+  * @author Rasmus Diekenbrock
+  */
+
+class CompLayoutBaseItem
+{
+public:
+
+	/** Constructor. Does nothing. */
+	CompLayoutBaseItem(CompLayoutBase * parentLayout);
+	/** Destructor. Does nothing. */
+	virtual ~CompLayoutBaseItem();
+	
+	/** This functions returns the size of the layout element. The size is measured in grid (8 Pixels).
+	 */
+	virtual int getSize() const = 0;
+	/** Returns the stretch factor of the layout element. In most case the factor is 0
+	 */
+	virtual int getStretchFactor() const = 0;
+	/** Positionized the layout element.
+	 *  @param size       The number of grid to distribute for stretch elements.
+	 *	@param sumStretch The sum of all stretch factors which have to split the size from above.
+	 *  @param startPos   The first position usable by this layout element.
+	 */
+	virtual int position(int & size, int & sumStretch, QPoint startPos) = 0;
+	
+	/** Returns the parent layout.*/
+	CompLayoutBase * getParent() const { return m_parentLayout; };
+  /** Returns the main layout. */
+  ComponentLayout * getBaseLayout() const;
+  /** Returns the related component. */
+  Component * getComponent() const;
+
+private:
+	CompLayoutBase * m_parentLayout;
+};
+
+//#######################################################################
+//#######################################################################
+
+/** A list which contains @see CompLayoutBaseItem elements.
+  * @author Rasmus Diekenbrock
+  */
+
+class CompLayoutList : public QList<CompLayoutBaseItem>
+{};
+
+
+#define FOR_EACH_LAYOUTITEM(_it_,_layoutList_)	\
+		for(QListIterator<CompLayoutBaseItem> _it_(_layoutList_);_it_.current();++_it_)
+
+
+//#######################################################################
+//#######################################################################
+
+
+/**Base class for component layouts.
+   This class manages the position of the connectors.
+  *@author Rasmus Diekenbrock
+  */
+
+
+class CompLayoutBase : public CompLayoutBaseItem
+{
+friend class ComponentLayout;
+
+public:
+	/** Constructs a layout element.
+			@param parentLayout The parent layout element.
+			@param orientation  Sets the connector orientation of this layout. Has to be equal as the
+			                    orientation of the parent layout.
+	 */
+	CompLayoutBase(CompLayoutBase * parentLayout, ConnOrientationType orientation);
+	/** The destructor. Deletes all layout element which where added to this element.
+	 */
+	virtual ~CompLayoutBase();
+
+  /** Adds a sub layout. The layout must have th same orientation.
+   */
+  void addLayout(CompLayoutBase * layout);
+  /** Adds a connector to the layout element. The orentation of the connector is set by the function.
+   *  It is not necassary to set a proper connector position (because this is the task auf this class).
+   *  Create a connector and add it, there is nothing else to do.
+   *  @param conn            The connector to add.
+   *  @param additionalSpace A additional space after the connector.
+   */
+  void addConnector(ConnectorBase * conn, int additionalSpace = 1);
+  /** Adds a space to the layout item.
+    * @param space Space size measured in grids.
+    */
+  void addSpace(int space);
+  /** Adds a stretch item to the layout element. The size of a stretch item depends on the unused space
+    * of the component side and the stretch factor.
+    * @param factor   The factor is relative to other stretch factors of the layout element. Higher stretch
+                      factors get more space.
+    */
+  void addStretch(int factor);
+  /** Adds a connector pack to the layout element. The required space of this element is count of connector *
+      (1 + space).
+    * @param connPack           The connector pack to add.
+    * @param additionalSpace    Space between two connectors and after the last connector.
+    */
+  void addConnectorPack(ConnectorPack * connPack, int additionalSpace = 1);
+
+  /** Returns the orientations of the element. */
+  ConnOrientationType getOrientation() const { return m_orientation; };
+
+  /** Returns the list of containing elements.*/
+  CompLayoutList * getItemList() const { return m_itemList; };
+	
+	/** This functions returns the size of the layout element. The size is measured in grid (8 Pixels).
+	 */
+	virtual int getSize() const;
+	/** Returns the stretch factor of the layout element. In most case the factor is 0
+	 */
+	virtual int getStretchFactor() const;
+	/** Positionized the layout element.
+	 *  @param size       The number of grid to distribute for stretch elements.
+	 *	@param sumStretch The sum of all stretch factors which have to split the size from above.
+	 *  @param startPos   The first position usable by this layout element.
+	 */
+	virtual int position(int & size, int & sumStretch, QPoint startPos);
+
+
+  /** Returns the parent layout. Returns a null pointer if the layout have no parent. */
+  CompLayoutBase * getParentLayout() const;
+  /** Returns the main layout. */
+  ComponentLayout * getBaseLayout() const;
+  /** Returns the related component. */
+  Component * getComponent() const;
+
+	/** Moves the position "pos" by "add" in the direction given by the orientation. */
+  void addPosition(QPoint & pos, int add);
+
+
+protected:
+	/** Constructor for internal purpose. */
+	CompLayoutBase(ComponentLayout * parent, ConnOrientationType orientation);
+	/** Sets the orientation. For internal use only. */
+  void setOrientation(ConnOrientationType orient) { m_orientation = orient; };
+
+private:
+	/** Contains the parent. */
+	union
+	{
+		CompLayoutBase * layout;
+		ComponentLayout * base;
+	} m_parent;
+	
+	/** Contains the orientation. */
+	ConnOrientationType m_orientation;
+	/** Conatins all child layout elements. */
+	CompLayoutList * m_itemList;
+	/** Some internal flags. */
+	Q_UINT32 m_flags;
+
+};
+
+//#######################################################################
+//#######################################################################
+
+/**A class for sub layout elements at the left side of the component.
+  *@author Rasmus Diekenbrock
+  */
+
+class ComponentLayoutLeft : public CompLayoutBase
+{
+public:
+	/** Constructs a sub layout element for the left component side. */
+	ComponentLayoutLeft(CompLayoutBase * parentLayout)
+	:	CompLayoutBase(parentLayout, CO_LEFT)
+	{};
+};
+
+/**A class for sub layout elements at the right side of the component.
+  *@author Rasmus Diekenbrock
+  */
+
+class ComponentLayoutRight : public CompLayoutBase
+{
+public:
+	/** Constructs a sub layout element for the right component side. */
+	ComponentLayoutRight(CompLayoutBase * parentLayout)
+	:	CompLayoutBase(parentLayout, CO_RIGHT)
+	{};
+};
+
+/**A class for sub layout elements at the top side of the component.
+  *@author Rasmus Diekenbrock
+  */
+
+class ComponentLayoutTop : public CompLayoutBase
+{
+public:
+	/** Constructs a sub layout element for the top component side. */
+	ComponentLayoutTop(CompLayoutBase * parentLayout)
+	:	CompLayoutBase(parentLayout, CO_TOP)
+	{};
+};
+
+/**A class for sub layout elements at the bottom side of the component.
+  *@author Rasmus Diekenbrock
+  */
+
+class ComponentLayoutBottom : public CompLayoutBase
+{
+public:
+	/** Constructs a sub layout element for the bottom component side. */
+	ComponentLayoutBottom(CompLayoutBase * parentLayout)
+	:	CompLayoutBase(parentLayout, CO_BOTTOM)
+	{};
+};
+
+//#######################################################################
+//#######################################################################
+
+/** A class for component layout. Add one element to your component. Add the layout elements to
+  * one of the four layouts. Each layout represents one side of the component.
+  * @author Rasmus Diekenbrock
+  */
+
+class ComponentLayout : public ComponentAddOn
+{
+  Q_OBJECT
+
+public:
+	/** Constructs a component layout.
+	  *
+	  * Construct a component layout, add connectors and other layout elements and forgot it :).
+	  * @param component Component for which the layout is.
+	  */
+	ComponentLayout(Component * component);
+	/** The destructor. Deletes also all added layout items. Do not use the destructor, this is done
+	  * by KSimus. */
+	~ComponentLayout();
+	
+	
+	/** Returns the layout for the left side. */
+	CompLayoutBase * getLeft() const   { return m_left; };
+	/** Returns the layout for the right side. */
+	CompLayoutBase * getRight() const  { return m_right; };
+	/** Returns the layout for the top side. */
+	CompLayoutBase * getTop() const    { return m_top; };
+	/** Returns the layout for the bottom side. */
+	CompLayoutBase * getBottom() const { return m_bottom; };
+	
+
+  /** Returns the minimum size. The default minimum size is QSize(5,5).*/
+  const QSize & getMinSize() const { return m_minSize; };
+  /** Sets the minimum size. */
+  void setMinSize(const QSize & minSize);
+  /** Sets the minimum size. */
+  void setMinSize(int width, int height) { setMinSize(QSize(width, height)); };
+	
+  /** Returns the current component size.*/
+  const QSize & getSize() const { return m_currentSize; };
+
+  /** Set component size fixed. If true, the layout dont change the size of the component. */
+  void setFixedSize(bool fixed);
+  /** Returns, if the component size is set to fix. */
+  bool isFixedSize() const { return m_fixedSize; };
+
+
+
+public slots:
+	/** Calculates uns sets new component size and connector positions. */
+	void updateLayout();
+	/** Actually calls @see updateLayout().*/
+	void slotResizeView();
+
+
+
+private:
+	CompLayoutBase * m_left;
+	CompLayoutBase * m_right;
+	CompLayoutBase * m_top;
+	CompLayoutBase * m_bottom;
+	QSize m_minSize;
+	QSize m_currentSize;
+	bool m_fixedSize;
+
+};
+
+
+
+#endif
