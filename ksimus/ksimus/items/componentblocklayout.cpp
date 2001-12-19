fc72c3cf6d0e47cf91f31cd7650e5815103c21cd
rasmus <rasmus>
2001-12-19 21:41:01 +0000

    * New: classes DoubleLayout, ComponentBlockLayout and ComponentControlBlock

---
+++ ksimus/ksimus/items/componentblocklayout.cpp
@@ -0,0 +1,151 @@
+/***************************************************************************
+                          componentblocklayout.cpp  -  description
+                             -------------------
+    begin                : Sun Dec 16 2001
+    copyright            : (C) 2001 by Rasmus Diekenbrock
+    email                : ksimus@gmx.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+// C-Includes
+
+// QT-Includes
+#include <qpainter.h>
+
+// KDE-Includes
+
+// Project-Includes
+#include "componentblocklayout.h"
+#include "component.h"
+#include "compview.h"
+#include "ksimaction.h"
+#include "resource.h"
+#include "ksimdebug.h"
+
+// Forward declaration
+
+
+#define FLAG_DRAW_FRAME 0x0001
+
+
+//#######################################################################
+//#######################################################################
+
+ComponentBlockLayout::ComponentBlockLayout(CompView * sheetView, ComponentLayout * compLayout, Orientation orient, QString blockName)
+	:	ComponentAddOn(sheetView->getComponent(), blockName),
+		DoubleLayout(compLayout, orient),
+		m_flags(FLAG_DRAW_FRAME),	
+		m_text(),
+		m_alignFlags(AlignCenter)
+{
+	getAction().disable(KSimAction::ALL);
+	getAction().enable(KSimAction::DRAWSHEETVIEW);
+}
+
+ComponentBlockLayout::~ComponentBlockLayout()
+{
+	KSIMDEBUG("ComponentBlockLayout::~ComponentBlockLayout()");
+}
+
+void ComponentBlockLayout::setDrawFrame(bool enable)
+{
+	if(enable)
+		m_flags |= FLAG_DRAW_FRAME;
+	else
+		m_flags &= ~FLAG_DRAW_FRAME;
+}
+
+bool ComponentBlockLayout::isDrawFrame() const
+{
+	return m_flags & FLAG_DRAW_FRAME;
+}
+
+void ComponentBlockLayout::drawSheetView (QPainter *p) const
+{
+	if (isDrawFrame() || !getText().isEmpty())
+	{	
+		QRect rect(DoubleLayout::getRect(false));
+		QRect rectView(getComponent()->getSheetView()->getPlace());
+	
+		rect.moveBy(rectView.topLeft().x(),rectView.topLeft().y());
+		rect.rLeft() ++;
+		rect.rTop() ++;
+		p->save();
+		p->setPen(QPen(black, 2));
+		p->setBrush(NoBrush);
+	
+		if (isDrawFrame())
+		{
+			p->drawRect(rect);
+		}
+		
+		if (!getText().isEmpty())
+		{
+			QFont newFont("helvetica",8);
+			p->setFont(newFont);
+			p->setPen(black);
+			p->drawText(rect, getAlign(), getText());
+		}
+		p->restore();
+	}
+}
+
+//#######################################################################
+//#######################################################################
+
+ComponentControlBlock::ComponentControlBlock(CompView * sheetView, ComponentLayout * compLayout, QString blockName)
+	: ComponentBlockLayout(sheetView, compLayout, Qt::Horizontal, blockName)
+{
+}
+	
+ComponentControlBlock::~ComponentControlBlock()
+{
+}
+
+void ComponentControlBlock::drawSheetView(QPainter *p) const
+{
+	if (isDrawFrame() || !getText().isEmpty())
+	{	
+		QRect rect(DoubleLayout::getRect(false));
+		QRect rectView(getComponent()->getSheetView()->getPlace());
+	
+		rect.moveBy(rectView.topLeft().x(),rectView.topLeft().y());
+		rect.rLeft() ++;
+		rect.rTop() ++;
+		p->save();
+		p->setPen(QPen(black, 2));
+//		p->setPen(black);
+		p->setBrush(NoBrush);
+	
+	
+		if (isDrawFrame())
+		{
+			p->moveTo(rect.bottomLeft() + QPoint(gridX/2,0));
+			p->lineTo(rect.bottomLeft() + QPoint(gridX/2,-gridY/2));
+			p->lineTo(rect.bottomLeft() + QPoint(0,-gridY/2));
+			p->lineTo(rect.topLeft());
+			p->lineTo(rect.topRight());
+			p->lineTo(rect.bottomRight() + QPoint(0,-gridY/2));
+			p->lineTo(rect.bottomRight() + QPoint(-gridX/2,-gridY/2));
+			p->lineTo(rect.bottomRight() + QPoint(-gridX/2,0));
+		}
+		
+		if (!getText().isEmpty())
+		{
+			QFont newFont("helvetica",8);
+			p->setFont(newFont);
+			p->setPen(black);
+			p->drawText(rect, getAlign(), getText());
+		}
+		p->restore();
+	}
+}
+
---
+++ ksimus/ksimus/items/componentblocklayout.h
@@ -0,0 +1,92 @@
+/***************************************************************************
+                          componentblocklayout.h  -  description
+                             -------------------
+    begin                : Sun Dec 16 2001
+    copyright            : (C) 2001 by Rasmus Diekenbrock
+    email                : ksimus@gmx.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef COMPONENTBLOCKLAYOUT_H
+#define COMPONENTBLOCKLAYOUT_H
+
+// C-Includes
+
+// QT-Includes
+#include <qstring.h>
+
+// KDE-Includes
+
+// Project-Includes
+#include "componentaddon.h"
+#include "doublelayout.h"
+
+
+// Forward declaration
+class CompView;
+
+
+//#######################################################################
+//#######################################################################
+
+/**Layout class for arrange connectors in a block on both sides of a component.
+  *@author Rasmus Diekenbrock
+  */
+
+class ComponentBlockLayout : public ComponentAddOn, public DoubleLayout
+{
+public:
+	ComponentBlockLayout(CompView * sheetView, ComponentLayout * compLayout, Orientation orient, QString blockName);
+	~ComponentBlockLayout();
+
+	/** Draws a rectangle frame and a text.
+		*/
+	virtual void drawSheetView (QPainter *p) const;
+
+  /** If enable is true a rectangle frame will be drawn.
+    * If false no frame will be drawn. */
+  void setDrawFrame(bool enable);
+  /** Returns true if a rectangle frame will be drawn. */
+  bool isDrawFrame() const;
+
+  /** Set a text which will be darw in the frame. */
+  void setText(const QString & text) { m_text = text; };
+  /** Returns the text which will be darw in the frame. */
+  QString getText() const { return m_text; };
+
+	/** Set the alignment of the drawn text. */
+	void setAlign(AlignmentFlags flags) { m_alignFlags = flags; };
+	/** Returns the alignment of the drawn text. */
+	AlignmentFlags getAlign() const { return m_alignFlags; };
+
+private:
+	/** Some internal flags. */
+	Q_UINT32 m_flags;
+	QString m_text;
+	AlignmentFlags m_alignFlags;
+};
+
+//#######################################################################
+//#######################################################################
+
+class ComponentControlBlock : public ComponentBlockLayout
+{
+public:
+	ComponentControlBlock(CompView * sheetView, ComponentLayout * compLayout, QString blockName = QString("Control Block"));
+	~ComponentControlBlock();
+
+	/** Draws a control block and a text.
+		*/
+	virtual void drawSheetView (QPainter *p) const;
+};
+
+
+#endif
