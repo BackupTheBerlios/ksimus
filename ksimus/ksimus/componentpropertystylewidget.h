bb1bd1deec6afe715cf6449979224daf3e71c584
rasmus <rasmus>
2002-02-04 22:50:21 +0000

    * Fix comment

--- ksimus/ksimus/componentpropertystylewidget.h
+++ ksimus/ksimus/componentpropertystylewidget.h
@@ -51,7 +51,7 @@ public:
 	 */
 	virtual void acceptPressed();
 	/** The function defaultPressed() is called, if user wants to set the default values.
-		You have to reimplement this slot, if you add new properties.
+		You have to reimplement this function, if you add new properties.
 	 */
 	virtual void defaultPressed();
 
