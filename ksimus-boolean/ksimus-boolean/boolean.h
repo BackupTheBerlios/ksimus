67811c34909d8f5eb2c149adb57c630cf5357680
rasmus <rasmus>
2004-03-06 15:36:32 +0000

    * New init function name

--- ksimus-boolean/ksimus-boolean/boolean.h
+++ ksimus-boolean/ksimus-boolean/boolean.h
@@ -50,11 +50,6 @@ class PackageInfo;
 /**  Enter the lower case package name. E.g. "boolean" */
 #define KSIMUS_PACKAGE_LOWER_NAME        "ksimus-boolean"
 
-/**  Enter the init function name. Format init_lib + lower case package name.
-     E.g. init_libboolean  (use "_" instead of "-") */
-#define KSIMUS_PACKAGE_INIT_FUNCTION     init_libksimus_boolean
-
-
 /******************************************************************************************
  ******************************************************************************************
  **
@@ -72,7 +67,7 @@ namespace KSimLibBoolean
 
 
 
-/** The init function. PACKAGE_INIT_FUNCTION is a macro which expands to the real function name. */
-extern "C" const PackageInfo * KSIMUS_PACKAGE_INIT_FUNCTION(KLocale * ksimusLocale);
+/** The init function. Don't change the name or signature! */
+extern "C" const PackageInfo * init_ksimus_package(KLocale * ksimusLocale);
 
 #endif
